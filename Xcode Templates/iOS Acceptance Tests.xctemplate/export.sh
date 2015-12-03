e6aff6d56c471dd3f0405603c953082573c53c99
Paul Stringer <paul.stringer@crowdmix.me>
2015-12-03 21:30:12 +0000

    Updates with the latest tested Xcode Project Template for iOS

--- Xcode Templates/iOS Acceptance Tests.xctemplate/export.sh
+++
@@ -1,8 +0,0 @@
-#
-#   Copyright 2014 Jonathan M. Reid. See LICENSE.txt
-#   Created by: Jon Reid, http://qualitycoding.org/
-#   Source: https://github.com/jonreid/XcodeCoverage
-#
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export | egrep '(INSTALL_DIR)|(PRODUCT_NAME)|(TARGET_BUILD_DIR)|(EXECUTABLE_FOLDER_PATH)' > ${DIR}/env.sh

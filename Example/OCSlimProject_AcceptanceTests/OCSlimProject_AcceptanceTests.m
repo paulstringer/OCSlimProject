17d3e0582f193a1a5af7b2878fb9083a23564a04
Paul Stringer <paul.stringer@crowdmix.me>
2015-12-05 18:40:05 +0000

    Renaming generated template xcode project files

--- Example/OCSlimProject_AcceptanceTests/OCSlimProject_AcceptanceTests.m
+++ Example/OCSlimProject_AcceptanceTests/OCSlimProject_AcceptanceTests.m
@@ -9,7 +9,7 @@
 #pragma GCC error "'CocoaSlim' pod not found. Add \" 'CocoaSlim', '~> 1.0' \" to the current targets pod configuration."
 #endif
 
-@interface CocoaSlim_AcceptanceTests : NSObject
+@interface OCSlimProject_AcceptanceTests : NSObject
 @end
-@implementation CocoaSlim_AcceptanceTests
+@implementation OCSlimProject_AcceptanceTests
 @end

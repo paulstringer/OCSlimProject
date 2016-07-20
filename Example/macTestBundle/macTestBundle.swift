309c0498fcd60a945be9ac12f17370155c5dc71e
Paul Stringer <paulstringer@mac.com>
2016-07-20 13:27:55 +0100

    Fixes an issue with Mac OS testing target not running

---
+++ Example/macTestBundle/macTestBundle.swift
@@ -0,0 +1,13 @@
+//
+//  macTestBundle.swift
+//  macTestBundle
+//
+//  Created by Paul Stringer on 20/07/2016.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import XCTest
+
+class macTestBundle: XCTestCase {
+    
+}

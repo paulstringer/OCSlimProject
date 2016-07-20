94bcb1e81559a265d6ccacdc1e7da8a1a60f5f99
Paul Stringer <paulstringer@mac.com>
2016-07-20 16:18:27 +0100

    Fixes missing Swift Libs due to missing entry in Run Search Paths

---
+++ Example/iOSAcceptanceTestsBundle/iOSAcceptanceTestsBundle.swift
@@ -0,0 +1,13 @@
+//
+//  iOSAcceptanceTestsBundle.swift
+//  iOSAcceptanceTestsBundle
+//
+//  Created by Paul Stringer on 20/07/2016.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import XCTest
+
+class iOSAcceptanceTestsBundle: XCTestCase {
+    
+}

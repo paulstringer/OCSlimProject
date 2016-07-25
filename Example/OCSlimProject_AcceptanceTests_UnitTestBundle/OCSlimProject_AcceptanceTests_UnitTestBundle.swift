6334571343b9a4dd5fe4e0ee574aab66705a5582
Paul Stringer <paulstringer@mac.com>
2016-07-25 20:40:07 +0100

    Recreating Unit Test Bundle with the most recent Template

---
+++ Example/OCSlimProject_AcceptanceTests_UnitTestBundle/OCSlimProject_AcceptanceTests_UnitTestBundle.swift
@@ -0,0 +1,25 @@
+//
+//  OCSlimProject_AcceptanceTests_UnitTestBundle.swift
+//  OCSlimProjectTestBundleSupport
+//
+//  Created by OCSlimProject on 25/07/2016.
+//  © MIT Licence
+//
+
+import XCTest
+
+class OCSlimProject_AcceptanceTests_UnitTestBundle: XCTestCase {
+    
+     /*
+     
+     XCTest bundles require that atleast one XCTestCase class exists within the target for it to be valid. This test case must remain part of your target for the bundle to be a valid XCTest bundle. It is not intended that you add additional tests to this class.
+     
+     */
+    
+    func testDoNotRemoveThisClassFromTestTarget() {
+        
+        XCTAssertTrue(true, "Removal of this XCTestCase file will likely result in an invalid XCTest bundle. A valid XCTestBundle requires atleast one real XCTestCase class to be part of the target for it to run.");
+        
+    }
+    
+}

091309712a32d11667514b55695e85998c73d913
Paul Stringer <paulstringer@mac.com>
2016-08-02 09:45:37 +0100

    Adds platform property to Podfile snippets. Simplifies the gcc error message to not include the entire Podfile snippet

--- Xcode Templates/Base Acceptance Testing Bundle.xctemplate/___OCSLIMPROJECTTESTBUNDLESUPPORTNAME___.m
+++ Xcode Templates/Base Acceptance Testing Bundle.xctemplate/___OCSLIMPROJECTTESTBUNDLESUPPORTNAME___.m
@@ -8,16 +8,13 @@
 
 #if __has_include(<OCSlimProjectTestBundleSupport/OCSlimProjectFitnesseTestsMain.h>)
 #else
-#pragma GCC error "'___OCSLIMPROJECTTESTBUNDLESUPPORTNAME___' pod was not found. Add the entry \"\
-target '___PACKAGENAMEASIDENTIFIER___' do \
-pod '___OCSLIMPROJECTTESTBUNDLESUPPORTNAME___' \
-end\"\
-to your Podfile configuration."
+#pragma GCC error "Target requires pod '___OCSLIMPROJECTTESTBUNDLESUPPORTNAME___'. Add the entry \"\pod '___OCSLIMPROJECTTESTBUNDLESUPPORTNAME___'\" to your Podfile configuration."
 #endif
 
 /* Copy and paste this entry to your projects Podfile and run 'pod install' or 'pod update' as necessary
 
  target '___PACKAGENAMEASIDENTIFIER___' do
+    platform :___POD_PLATFORM_IDENTIFIER___
     pod '___OCSLIMPROJECTTESTBUNDLESUPPORTNAME___'
  end
 

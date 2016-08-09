f41360fc0360e0f8b65b7051ca536f7c9112cd8b
Paul Stringer <paulstringer@mac.com>
2016-08-09 08:27:49 +0100

    Adding Unit Test Bundle for iOS Acceptance Tests

---
+++ Example/OCSlimProject_AcceptanceUnitTestBundle/OCSlimProject_AcceptanceUnitTestBundle.m
@@ -0,0 +1,30 @@
+//
+//  OCSlimProject_AcceptanceUnitTestBundle.m
+//  OCSlimProjectTestBundleSupport
+//
+//  Created by OCSlimProject on 09/08/2016.
+//  MIT License
+//  © 2016 Paul Stringer
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OCSlimProject_AcceptanceUnitTestBundle : XCTestCase
+
+@end
+
+@implementation OCSlimProject_AcceptanceUnitTestBundle
+
+/**
+ 
+ XCTest bundles require that atleast one XCTestCase class exists within the target for it to be valid. This test case must remain part of your target for the bundle to be a valid XCTest bundle. It is not intended that you add additional tests to this class but feel free to disable them.
+ 
+ */
+
+- (void)testDoNotRemoveThisClassFromTestTarget {
+
+    XCTAssertTrue(YES, @"Removal of this XCTestCase file will likely result in an invalid XCTest bundle. A valid XCTestBundle requires atleast one real XCTestCase class to be part of the target for it to run.");
+    
+}
+
+@end

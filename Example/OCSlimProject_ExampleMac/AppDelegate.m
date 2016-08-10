5c7cd41672c5077d1cfb16abbb430a7c17ff7ad2
Paul Stringer <paulstringer@mac.com>
2016-08-10 17:06:21 +0100

    Adding Acceptance Unit Test Bundle to Mac Example and Building in CI

---
+++ Example/OCSlimProject_ExampleMac/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  OCSlimProject_ExampleMac
+//
+//  Created by Paul Stringer on 10/08/2016.
+//  Copyright © 2016 CleanKit. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
---
+++ Example/OCSlimProject_ExampleMac/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  OCSlimProject_ExampleMac
+//
+//  Created by Paul Stringer on 10/08/2016.
+//  Copyright © 2016 CleanKit. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}

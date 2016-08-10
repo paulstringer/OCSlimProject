835de75bb6c8e16752ddcd523bc9972fb9cb620c
Paul Stringer <paulstringer@mac.com>
2016-08-10 19:53:48 +0100

    Adding Verbosity to see what's in the generated test report file in the log

--- Pod/Support/SharedSupport/ocsp-generate-fitnesse-test-report.sh
+++ Pod/Support/SharedSupport/ocsp-generate-fitnesse-test-report.sh
@@ -4,7 +4,7 @@ TEST_REPORT_FILE_PATH=$2
 function main {
 	echo "[OCSP_TEST] Fitnesse Test Suite=$FITNESSE_SUITE_NAME"	
 	echo "[OCSP_TEST] Destination Report File Path=$TEST_REPORT_FILE_PATH"
-	${PROJECT_DIR}/LaunchFitnesse -d "${PROJECT_DIR}" -b "$TEST_REPORT_FILE_PATH" --test "$FITNESSE_SUITE_NAME" >&1 
+	${PROJECT_DIR}/LaunchFitnesse -d "${PROJECT_DIR}" -b "$TEST_REPORT_FILE_PATH" --verbose --test "$FITNESSE_SUITE_NAME" >&1 
 }
 
 main

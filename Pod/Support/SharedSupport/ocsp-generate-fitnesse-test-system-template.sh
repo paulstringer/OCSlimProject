9d4286098874965db7d5fb63b5f51359ec6f5214
Paul Stringer <paulstringer@mac.com>
2016-08-15 21:56:05 +0100

    Cleaning up ios-sim output to validate correct build pass/fail results

--- Pod/Support/SharedSupport/ocsp-generate-fitnesse-test-system-template.sh
+++ Pod/Support/SharedSupport/ocsp-generate-fitnesse-test-system-template.sh
@@ -5,6 +5,10 @@ ENV_FILE_PATH=%s
 ERROR_LOG_FILE=%s
 
 $TEST_RUNNER_SCRIPT $ENV_FILE_PATH $SLIM_PORT 2> >(tee $ERROR_LOG_FILE >&2)
-if [ -z "$(cat $ERROR_LOG_FILE)" ] ; then 
+
+ERROR_LOG_NO_WHITESPACE=$(more "$ERROR_LOG_FILE" | sed 's/ *$//')
+	
+if [ -z "$ERROR_LOG_NO_WHITESPACE" ]; then 
 	rm $ERROR_LOG_FILE
+	echo "[OCSP_RUN] Slim Test System Exited Normally"
 fi

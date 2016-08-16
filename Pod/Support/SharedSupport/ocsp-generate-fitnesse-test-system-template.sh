29fe3bc162f5270e05638822ee3244eec84b9efa
Paul Stringer <paulstringer@mac.com>
2016-08-16 20:52:39 +0100

    [DEV] Added extra logging

--- Pod/Support/SharedSupport/ocsp-generate-fitnesse-test-system-template.sh
+++ Pod/Support/SharedSupport/ocsp-generate-fitnesse-test-system-template.sh
@@ -11,4 +11,6 @@ ERROR_LOG_NO_WHITESPACE=$(more "$ERROR_LOG_FILE" | sed 's/ *$//')
 if [ -z "$ERROR_LOG_NO_WHITESPACE" ]; then 
 	rm $ERROR_LOG_FILE
 	echo "[OCSP_RUN] Slim Test System Exited Normally"
+else
+	echo "[OCSP_RUN] Slim Test System Exited Abnormally With Reported Errors"
 fi

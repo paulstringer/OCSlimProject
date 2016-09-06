cb2a4dd8ce1915ade054b54b3cec3cfd2d299669
Paul Stringer <paulstringer@mac.com>
2016-09-06 11:51:48 +0100

    Breaks dependancy errors logging onto two lines to fix line breaks in CI / Command Line Builds

--- Pod/Support/SharedSupport/ocsp-generate-fitnesse-test-system-template.sh
+++ Pod/Support/SharedSupport/ocsp-generate-fitnesse-test-system-template.sh
@@ -10,7 +10,8 @@ ERROR_LOG_NO_WHITESPACE=$(more "$ERROR_LOG_FILE" | sed 's/ *$//')
 	
 if [ -z "$ERROR_LOG_NO_WHITESPACE" ]; then 
 	rm $ERROR_LOG_FILE
-	echo "[OCSP_RUN] Slim Test System Exited Normally"
+	echo >&1 "[OCSP_RUN] Slim Test System Exited Normally"
 else
-	echo "[OCSP_RUN] Slim Test System Exited Abnormally With Reported Errors"
+	echo "[OCSP_RUN] Slim Test System Exited Abnormally With Reported Errors" >> "$ERROR_LOG_FILE"
+	echo >&2 "[OCSP_RUN] Slim Test System Exited Abnormally With Reported Errors"
 fi

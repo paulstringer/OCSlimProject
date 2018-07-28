6b26cd2c987f26798a9826f8c9daa7d84d14e1b5
Paul Stringer <paulstringer@mac.com>
2018-07-28 13:46:07 +0100

    Add --help with usage for simctl script

--- update_pod_testrunner_sripts.sh
+++ update_pod_testrunner_sripts.sh
@@ -7,22 +7,41 @@
 POD_SUPPORT_PATH=Pod/Support
 POD_LOCAL_INSTALL_SUPPORT_PATH=Example/Pods/OCSlimProject/Pod/Support
 
+
+# Subroutines
+
+copyFromPodSourceToLocalInstall() {
+	copy $POD_SUPPORT_PATH $POD_LOCAL_INSTALL_SUPPORT_PATH
+	exit 0
+}
+
+copyFromLocalInstallToPodSource() {
+	copy $POD_LOCAL_INSTALL_SUPPORT_PATH $POD_SUPPORT_PATH 
+	exit 0
+}
+
+copy() {
+	echo "Copying OCSlimProject Pod support files from location $1 -> $2"
+	cp $1/iOS/* $2/iOS
+	cp $1/OSX/* $2/OSX
+}
+
+# Main
+
 while [ "$1" != "" ]; do
 
     case $1 in
 		
-		-i | --install )		copy $POD_SUPPORT_PATH POD_LOCAL_INSTALL_SUPPORT_PATH
+		-i | --install )		copyFromPodSourceToLocalInstall
                                 ;;
 								
-		-r | --update-reverse )	copy POD_LOCAL_INSTALL_SUPPORT_PATH $POD_SUPPORT_PATH 
+		-r | --update-reverse )	copyFromLocalInstallToPodSource
 								;;
 		
 		esac
 		shift
 done
 
+# Default behaviour if no arg provided
 
-copy() {
-	cp $1/iOS/* $2/iOS
-	cp $1/OSX/* $2/OSX
-}
+copyFromPodSourceToLocalInstall

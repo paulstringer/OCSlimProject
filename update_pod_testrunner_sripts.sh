9aef266e28ca482cb0eb811c1ee0bb44b80dc3d7
Paul Stringer <paulstringer@mac.com>
2018-07-28 13:00:42 +0100

    Signposts where to find error logs in regular logs

--- update_pod_testrunner_sripts.sh
+++ update_pod_testrunner_sripts.sh
@@ -1,3 +1,36 @@
 #!/bin/sh
-cp Example/Pods/OCSlimProject/Pod/Support/iOS/* Pod/Support/iOS
-cp Example/Pods/OCSlimProject/Pod/Support/OSX/* Pod/Support/OSX
+
+####################################
+# Command Line Argument Processing #
+####################################
+ACTION=UPDATE-POD-INSTALL
+
+while [ "$1" != "" ]; do
+
+    case $1 in
+		
+		-i | --install )		shift
+								ACTION=UPDATE-POD-INSTALL
+                                ;;
+								
+		-r | --update-reverse )	shift
+								ACTION=COPY-FROM-LOCAL-POD-INSTALL
+								;;
+		
+		esac
+		shift
+done
+
+case "$ACTION" in
+
+	UPDATE-POD-INSTALL) 
+		cp Pod/Support/iOS/* Example/Pods/OCSlimProject/Pod/Support/iOS
+		cp Pod/Support/OSX/* Example/Pods/OCSlimProject/Pod/Support/OSX
+		;;
+		
+	COPY-FROM-LOCAL-POD-INSTALL)
+		cp Example/Pods/OCSlimProject/Pod/Support/iOS/* Pod/Support/iOS
+		cp Example/Pods/OCSlimProject/Pod/Support/OSX/* Pod/Support/OSX
+		;;
+		
+esac

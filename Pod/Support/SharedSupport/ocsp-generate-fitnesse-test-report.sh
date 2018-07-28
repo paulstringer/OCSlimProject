8e9dd6cd12e65f484ebade39c65fba116fff9f59
Paul Stringer <paulstringer@mac.com>
2018-07-28 15:31:07 +0100

    Shutdown Fitnesse before running tests from Xcode

--- Pod/Support/SharedSupport/ocsp-generate-fitnesse-test-report.sh
+++ Pod/Support/SharedSupport/ocsp-generate-fitnesse-test-report.sh
@@ -6,6 +6,7 @@ RETURN_CODE=0
 function main {
 	echo "[OCSP_TEST] Fitnesse Test Suite=$FITNESSE_SUITE_NAME"	
 	echo "[OCSP_TEST] Destination Report File Path=$TEST_REPORT_FILE_PATH"
+	${PROJECT_DIR}/LaunchFitnesse --shutdown 2>&1 >/dev/null
 	${PROJECT_DIR}/LaunchFitnesse -d "${PROJECT_DIR}" -b "$TEST_REPORT_FILE_PATH" --test "$FITNESSE_SUITE_NAME" --verbose >&1 
 }
 

c32686bc8f15e8db660ac3563630be2ffde9bd6d
paulstringer <pstringer@equalexperts.com>
2018-08-02 21:01:45 +0100

    Refactor logging message from test runner script

--- Pod/Support/SharedSupport/ocsp-generate-fitnesse-test-system-template.sh
+++ Pod/Support/SharedSupport/ocsp-generate-fitnesse-test-system-template.sh
@@ -7,10 +7,20 @@ ERROR_LOG_FILE=%s
 $TEST_RUNNER_SCRIPT $ENV_FILE_PATH $SLIM_PORT 2> >(tee $ERROR_LOG_FILE >&2)
 
 ERROR_LOG_NO_WHITESPACE=$(more "$ERROR_LOG_FILE" | sed 's/ *$//')
-	
+
+log() {
+	echo "[OCSP_RUN]: INFO $1"
+}
+
+error() {
+	>&2 echo "[OCSP_RUN]: ERROR $1"
+	exit 1
+}
+
 if [ -z "$ERROR_LOG_NO_WHITESPACE" ]; then 
 	rm $ERROR_LOG_FILE
-	echo "[OCSP_RUN] Slim Test System Exited Normally"
+	log "Slim Test System Exited OK"
+	exit 0
 else
-	echo "[OCSP_RUN] Slim Test System Exited Abnormally With Reported Errors"
+	error "Slim Test System Exited Abnormally With Reported Errors"
 fi

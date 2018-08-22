8c686ea850adb13ce73248a1dc8e3b5e55bf8663
Paul Stringer <paulstringer@mac.com>
2018-08-22 05:29:46 +0100

    Generated logfile name not uniqued

--- Pod/Support/SharedSupport/ocsp-logger-tests.sh
+++ Pod/Support/SharedSupport/ocsp-logger-tests.sh
@@ -2,6 +2,7 @@
 source '../../../assert.sh/assert.sh'
 
 LOGPATH=$(./ocsp-logger.sh new-log-file)
+LOGBASENAME=$(basename $LOGPATH)
 LOGDIR=$(dirname $LOGPATH)
 
 assert_not_empty "$LOGPATH"
@@ -10,4 +11,10 @@ assert_not_empty "$LOGPATH"
 test -d "$LOGDIR"
 assert_eq 0 $?
 [ "$?" == 1 ] && log_failure "new-log-file '$LOGDIR' is not a valid directory"
+
+expected="slim_system.log"
+assert_eq "$LOGBASENAME" "$expected"
+[ "$?" == 1 ] && log_failure "new-log-file '$LOGBASENAME' is not the expected '$expected' filename"
+
+
 
--- Pod/Support/SharedSupport/ocsp-logger.sh
+++ Pod/Support/SharedSupport/ocsp-logger.sh
@@ -7,7 +7,7 @@ function main() {
     if [ "$ACTION" == "LOG_PATH" ]; then
 	    local LOGDIR="Logs"
 	    local LOGPATH="$(pwd)/$LOGDIR"
-	    local LOGFILENAME="$(date +%Y%m%d-%H%M%S).log"
+	    local LOGFILENAME="slim_system.log"
 	    local LOGFILE="$LOGPATH/$LOGFILENAME"
 	    mkdir -p $LOGPATH
         echo "$LOGFILE"
